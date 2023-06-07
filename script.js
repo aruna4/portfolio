cc243b6aaec133a7b56619c9b7223da7a995a6ae
aruna4 <87902885+aruna4@users.noreply.github.com>
2023-06-07 16:59:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function myFunction() {
     var element = document.getElementById("nav-list");
     element.classList.toggle("navlist-style");
 }
+
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
